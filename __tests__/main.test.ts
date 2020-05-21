e13e54824f591ab3a0a1071529fd035be5a15f96
yaananth (Yash) <yaananth@github.com>
2020-05-21 09:25:34 -0400

    changes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,7 +2,6 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-
 test('test runs', () => {
   process.env['INPUT_SLASH'] = '500'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
