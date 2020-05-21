5e8e5ba1ca7dba33aad18938ce84bfdb5fa610ea
yaananth (Yash) <yaananth@github.com>
2020-05-21 08:54:40 -0400

    changes

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,3 +2,12 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
+
+test('test runs', () => {
+  process.env['INPUT_SLASH'] = '500'
+  const ip = path.join(__dirname, '..', 'lib', 'main.js')
+  const options: cp.ExecSyncOptions = {
+    env: process.env
+  }
+  console.log(cp.execSync(`node ${ip}`, options).toString())
+})
